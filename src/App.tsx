98eecc01ee32a694dcfef1645b6de14a7874ea75
sihyeong <syhy7051@gmail.com>
2024-03-23 00:50:59 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,115 @@
+import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 10px;
+`;
+
+const Box = styled(motion.div)`
+  background-color: rgba(255, 255, 255, 0.7);
+  border-radius: 10px;
+  height: 250px;
+  width: 400px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Circle = styled(motion.div)`
+  background-color: white;
+  height: 60px;
+  width: 60px;
+  border-radius: 50px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const overlay = {
+  hidden: { backgroundColor: "rgba(0, 0, 0, 0)" },
+  visible: { backgroundColor: "rgba(0, 0, 0, 0.5)" },
+  exit: { backgroundColor: "rgba(0, 0, 0, 0)" },
+};
+
+const boxVar = {
+  hover: { scale: 1.1 },
+  animation: (position: string) => ({
+    transformOrigin: position,
+  }),
+};
+
+function App() {
+  const [id, setId] = useState<null | string>(null);
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked((prev) => !prev);
+
+  return (
+    <Wrapper>
+      <Grid>
+        <Box
+          custom={"right bottom"}
+          onClick={() => setId("1")}
+          key={"1"}
+          layoutId={"1"}
+          variants={boxVar}
+          whileHover="hover"
+          animate="animation"
+        />
+        <Box>{clicked ? <Circle layoutId="circle" /> : null}</Box>
+        <Box>{!clicked ? <Circle layoutId="circle" /> : null}</Box>
+        <Box
+          custom={"left top"}
+          onClick={() => setId("2")}
+          key={"2"}
+          layoutId={"2"}
+          variants={boxVar}
+          whileHover="hover"
+          animate="animation"
+        />
+      </Grid>
+      <button onClick={toggleClicked}>Switch</button>
+      <AnimatePresence>
+        {id ? (
+          <Overlay
+            variants={overlay}
+            onClick={() => setId(null)}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+          >
+            <Box
+              layoutId={id}
+              style={{
+                width: 400,
+                height: 300,
+                backgroundColor: "rgba(255, 255, 255, 1)",
+              }}
+            />
+          </Overlay>
+        ) : null}
+      </AnimatePresence>
+    </Wrapper>
+  );
+}
+
+export default App;
